910be26852178fa1885c0fc53a399d4944a95ed4
Jens Laufer <jens.laufer@solytics.de>
2025-12-30 17:28:01 +0100

    added all html pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { glob } from 'glob'
+import path from 'path'
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
     ],
+    build: {
+        rollupOptions: {
+            input: glob.sync('*.html').reduce((acc, file) => {
+                const name = path.basename(file, '.html')
+                acc[name] = path.resolve(__dirname, file)
+                return acc
+            }, {})
+        }
+    }
 })
